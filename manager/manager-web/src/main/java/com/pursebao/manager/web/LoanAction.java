e7a02f588670ad45d3d828539617dbf240ef8b2a
sailinxing <1176218450@qq.com>
2018-03-12 20:14:50 +0800

    Initial commit

---
+++ manager/manager-web/src/main/java/com/pursebao/manager/web/LoanAction.java
@@ -0,0 +1,11 @@
+package com.pursebao.manager.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2018/3/12.
+ */
+@Controller
+public class LoanAction {
+
+}
